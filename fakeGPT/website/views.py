8d3089fa6a57252685ff91a69fe6acc971d91024
VIGHNESH M S <princevighnesh71@gmail.com>
2024-05-13 21:36:10 +0530

    homepage

--- fakeGPT/website/views.py
+++ fakeGPT/website/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    lang_list = ['c', 'cpp', 'css', 'html',
+                 'java', 'javascript', 'php', 'python']
+
+    if request.method == "POST":
+        code = request.POST['code']
+        lang = request.POST['lang']
+        if lang == "Select Programming Language":
+            messages.success(request, "Hey! You forgot")
+            return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
+        return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
+
+    return render(request, 'home.html', {'lang_list': lang_list})
