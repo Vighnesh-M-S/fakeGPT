7d101e7c2d2c573c617e40275a3df584151ceb7e
VIGHNESH M S <princevighnesh71@gmail.com>
2024-05-13 22:03:26 +0530

    openai

--- fakeGPT/website/views.py
+++ fakeGPT/website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
+import openai
 
 
 def home(request):
@@ -8,10 +9,29 @@ def home(request):
 
     if request.method == "POST":
         code = request.POST['code']
-        lang = request.POST['lang']
-        if lang == "Select Programming Language":
+        if (request.POST['lang']) == "Select Programming Language":
             messages.success(request, "Hey! You forgot")
-            return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
-        return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
+            return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': request.POST['lang']})
+        else:
+
+            openai.api_key = "sk-proj-u4DuGsyYi9NrME1Qa6SfT3BlbkFJNl2D05okyWSnEif1x617"
+            openai.Model.list()
+
+            try:
+                response = openai.Completion.create(
+                    engine='text-davinci-003',
+                    prompt="Respond only with code. Fix this " +
+                    {lang} + " code: " + {code},
+
+                    temperature=0,
+                    max_tokens=1000,
+                    top_p=1.0,
+                    frequency_penalty=0.0,
+                    presence_penalty=0.0,
+                )
+                return render(request, 'home.html', {'lang_list': lang_list, 'response': response, 'lang': request.POST['lang']})
+
+            except Exception as e:
+                return render(request, 'home.html', {'lang_list': lang_list, 'code': e, 'lang': request.POST['lang']})
 
     return render(request, 'home.html', {'lang_list': lang_list})
